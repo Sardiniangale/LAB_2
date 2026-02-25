9012431c83c7e31b5b8fcf683dc7d90ee7630ff1
Giacomo Solinas <sardiniangale24@gmail.com>
2026-02-25 15:09:47 +0100

    Reorganize repository: flatten structure and group files by experiment

---
+++ Esperienza_11/punto_4.py
@@ -0,0 +1,39 @@
+import numpy as np
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+import scipy.stats as st
+
+Filename=("/home/studentelab2/dir_mercoledi/Esperienza_11/holy_fish2.txt") #reads from here
+
+#loding
+V_in, delta_in, V_out, delta_out, f_j = np.loadtxt(Filename,unpack=True)
+
+print(f_j, V_in, delta_in, V_out, delta_out)
+
+
+G = V_out/V_in #gain
+
+delta_G = (V_out * delta_in + V_in * delta_out)/V_in**2
+
+
+#function
+def gain(f_j, A, f_t):
+    return A/np.sqrt(1+(f_j/f_t)**2)
+
+
+#fit
+
+popt, pcov = curve_fit(gain, f_j, G, sigma=delta_G, absolute_sigma=False)
+A, f_t = popt
+sigma_A, sigma_f_t = np.sqrt(np.diag(pcov))
+
+#grafico
+
+plt.figure()
+plt.loglog(f_j, G, marker = 'o', linestyle = 'None')
+plt.loglog(f_j, gain(f_j, *popt))
+plt.xlabel("F [Hz]")
+plt.ylabel("Gain")
+plt.title("Gain VS F [Hz]")
+plt.legend()
+plt.show()
